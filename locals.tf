a8cfea03f57c2ecafc21c51a56d891a231d46179
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-19 14:49:22 +0530

    added rule

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals{
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
     alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
-    host_header = var.component == "frontend" ? "${var.component}-${var.environment}-${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+    host_header = var.component == "frontend" ? "${var.component}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
     common_tags = {
         project = var.project
         Environment = var.environment
